c64cf3c674daaf301c3cc82a525fe189e9cca631
Titouax <titouan.dessus@gmail.com>
2018-07-31 01:07:22 +0200

    Vos pages perso les copaigues

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,28 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def about
+
+  end
+
+  def contact
+
+  end
+
+  def francois
+
+  end
+
+  def ani
+
+  end
+
+  def titouan
+
+  end
+
+  def groupe
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
   root to: 'static_pages#home'
+  get '/contact', to: 'static_pages#contact'
+  get '/about', to: 'static_pages#about'
+  get 'about/francois', to: 'static_pages#francois'
+  get 'about/ani', to: 'static_pages#ani'
+  get 'about/titouan', to: 'static_pages#titouan'
+  get 'about/JR', to: 'static_pages#JR'
   get '/new', to: 'users#new'
   post '/new', to: 'users#new_post'
   get '/error', to: 'users#error', as: 'error'
