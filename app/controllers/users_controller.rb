84ee86f5bf95838bc3ac4750529fb97dc852cc9d
TheFSilver <40010470+TheFSilver@users.noreply.github.com>
2018-08-01 01:21:53 +0200

    update commentaires

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,15 +9,22 @@ class UsersController < ApplicationController
     user.username = params["username"]
     user.bio = params["bio"]
     user.save
+    
+    # Si un pseudo inclut un espace, l'user est redirige sur une page error
     if user.username.include?(" ")
       redirect_to "/error2"
+      
+    # Si l’user est valide, il a un id et on le redirige vers la page profil
     elsif user.id != nil
-      redirect_to "/users/#{User.last.username}"
+      redirect_to "/users/#{User.last.username}"  
+     
+    # sinon on l’envoie vers une page error
     else
       redirect_to "/error"
     end
   end
 
+  # Recupere l’username entré et stocke les infos de l’user actuel dans des variables
   def show
     @username = params[:username]
     @id = User.find_by(username: @username).id
